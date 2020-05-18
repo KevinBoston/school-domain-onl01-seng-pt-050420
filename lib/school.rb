5fe360ba799436970b31e9fbc6561fcc1f8ffc42
Kevin Boston <kbos22@gmail.com>
2020-05-18 13:22:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,15 @@
 # code here!
 class School
   attr_accessor :name, :roster
+  
   def initialize(school_name)
     @name = school_name
     @roster = {}
   end
   
   def add_student=(name, grade)
-    @roster[grade] = []
-    @roster[grade] << name
+    @roster[grade.to_s] = []
+    @roster[grade.to_s] << name
   end
   
   def roster
