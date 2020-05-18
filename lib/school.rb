ceec8e2201f46609c0265da57da57d25f5dd8e1c
Kevin Boston <kbos22@gmail.com>
2020-05-18 13:15:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+class School
+  def initialize(school_name)
+    @school_name
+    @roster = hash.new 
+  end
+  
+  def add_student=(name, grade)
+    @roster[:grade] = []
+    @roster[:grade] << name
+  end
+  
+  def roster
+    puts @roster    
+  end
+  
+  def grade(num)
+    puts @roster[:num]
+  end
+  def sort 
+    
+  end
+  
+end
